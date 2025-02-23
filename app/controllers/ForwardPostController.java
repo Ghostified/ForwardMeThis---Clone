ea40051c1f45ff985af1f2badf4f36731d05d77b
Ghostified <bransonallan@gmail.com>
2025-02-23 23:42:45 +0300

    initial commit

---
+++ app/controllers/ForwardPostController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import play.mvc.Controller;
+
+/**
+ * Handles all the post requests and sending of emails
+ */
+
+public class ForwardPostController extends Controller {
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+}
---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := """forward-me-this"""
+organization := "com.forwardmethis"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.16"
+
+libraryDependencies += guice
