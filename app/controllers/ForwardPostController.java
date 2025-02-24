9812a7e412bcbb97960128a6ba5fdcecab085c5a
Ghostified <bransonallan@gmail.com>
2025-02-24 23:23:46 +0300

    completed a database connection on the configuration file

--- app/controllers/ForwardPostController.java
+++ app/controllers/ForwardPostController.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import org.apache.commons.validator.routines.EmailValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * Handles all the post requests and sending of emails
@@ -10,5 +12,19 @@ import play.mvc.Controller;
 
 public class ForwardPostController extends Controller {
     private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    //Method to handle the post requests from the html forms
+    public Result processEmailRequest(String emailAddress ){
+        logger.debug("Processing Email Request: {}" , emailAddress);
+
+        //Validate email from the database to submit forms
+        if (!EmailValidator.getInstance().isValid(emailAddress)) {
+            logger.warn("Invalid Email Request: {}", emailAddress);
+            return badRequest();
+        }
+
+
+        return  ok();
+    }
 
 }
--- build.sbt
+++ build.sbt
@@ -8,3 +8,12 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.13.16"
 
 libraryDependencies += guice
+
+//Validate emails
+libraryDependencies += "commons-validator" %  "commons-validator" % "1.9.0"
+
+//Database Support
+libraryDependencies ++= Seq(
+  javaJdbc,
+  "mysql" % "mysql-connector-java" % "8.0.14"
+)
